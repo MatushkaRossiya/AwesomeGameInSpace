e9e37c9d694cc2ea9dc74ef76494f97fdd905e06
romanchom <romanchom@gmail.com>
2014-03-29 14:38:29 +0100

    - Door test

---
+++ Game/Assets/PushButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushButton : MonoBehaviour {
+    public Door door;
+
+    void OnCollisionEnter(Collision col)
+    {
+        door.Toggle();
+    }
+}
