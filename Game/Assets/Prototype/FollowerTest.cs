5542c8cdc919abf2896f4e2869130b88db4435b5
romanchom <romanchom@gmail.com>
2014-03-29 23:08:03 +0100

    - Dalek model added. - EXTERMINATE!!!!!

--- Game/Assets/Prototype/FollowerTest.cs
+++ Game/Assets/Prototype/FollowerTest.cs
@@ -4,15 +4,26 @@ using System.Collections;
 public class FollowerTest : MonoBehaviour {
     NavMeshAgent agent;
     GameObject player;
+    AudioSource audio;
+
+    float time;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<PlayerController>().gameObject;
+        audio = GetComponent<AudioSource>();
+        time = Time.fixedTime + Random.Range(1.0f, 5.0f);
+        audio.pitch = Random.Range(0.9f, 1.1f);
 	}
 
-    void Update()
+    void FixedUpdate()
     {
         agent.SetDestination(player.transform.position);
+        if (Time.fixedTime > time)
+        {
+            audio.Play();
+            time += Random.Range(10.0f, 20.0f);
+        }
     }
 }
