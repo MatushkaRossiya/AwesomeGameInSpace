cb7eddd84836fcc3ba51309b9b8b89b1ca090748
romanchom <romanchom@gmail.com>
2014-04-11 16:40:07 +0200

    - added full auto to AK47 - added limited clip to AK47

--- Game/Assets/GunPlaceholder/AK47Shooter.cs
+++ Game/Assets/GunPlaceholder/AK47Shooter.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class AK47Shooter : MonoBehaviour {
     public AudioClip pewSound;
-    void Update() {
+    public AudioClip outOfAmmoSound;
 
+    [Range(0.1f, 0.5f)]
+    public float shotPeriod;
+    public int clipSize;
+    
+
+    private float nextShot;
+    private int currentClip;
+
+    void Start() {
+        nextShot = Time.fixedTime;
+        currentClip = clipSize;
+    }
+
+    void Update() {
         RaycastHit hitInfo;
         bool hit = Physics.Raycast(transform.position, transform.up, out hitInfo);
         if (hit) {
@@ -12,14 +26,29 @@ public class AK47Shooter : MonoBehaviour {
         } else {
             Debug.DrawRay(transform.position, transform.up * 10000.0f, Color.red);
         }
-        if (Input.GetMouseButtonDown(0)) {
-            audio.PlayOneShot(pewSound);
-            if (hit) {
-                Damageable damagable = hitInfo.collider.GetComponent<Damageable>();
-                if (damagable != null) {
-                    damagable.DealDamage(transform.up * 11.0f);
+        if (Time.fixedTime >= nextShot && Input.GetMouseButton(0)) {
+            nextShot = Time.fixedTime + shotPeriod;
+            if (currentClip <= 0) {
+                audio.PlayOneShot(outOfAmmoSound);
+            } else {
+                --currentClip;
+                audio.PlayOneShot(pewSound);
+                if (hit) {
+                    Damageable damagable = hitInfo.collider.GetComponent<Damageable>();
+                    if (damagable != null) {
+                        damagable.DealDamage(transform.up * 11.0f);
+                    }
                 }
-            } 
+            }
         }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            currentClip = clipSize;
+        }
+    }
+
+    void OnGUI() {
+        int sw = Screen.width;
+        int sh = Screen.height;
+        GUI.Label(new Rect(sw * 0.8f, sh * 0.8f, sw * 0.2f, sh * 0.2f), "AMMO: " + currentClip);
     }
 }
--- Game/Assets/Player/HUD.cs
+++ Game/Assets/Player/HUD.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class HUD : MonoBehaviour {
     void OnGUI() {
-        GUI.Label(new Rect(10, 10, 200, 50), "Stuff count: " + DataBank.stuffCount);
+        //GUI.Label(new Rect(10, 10, 200, 50), "Stuff count: " + DataBank.stuffCount);
     }
 }
