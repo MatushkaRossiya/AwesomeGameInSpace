efc80f1ca2cd76bdcb91df8f45f302c184665c28
jakubdziworski <jakub.dziworski@gmail.com>
2014-05-06 13:41:38 +0200

    q fix

--- Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class PlayerHealth : Damageable {
 	public float health;
+	private float maxHealth;
 	private float currentHealth{
 		get{
 			return health;
 		}
 		set{
-			GameObject.FindObjectOfType<HUD>().GetComponent<HUD>().updateHealth(value/health);	//powiadamia hud o zmianie zycia
 			health = value;
+			GameObject.FindObjectOfType<HUD>().GetComponent<HUD>().updateHealth(health/maxHealth);	//powiadamia hud o zmianie zycia
 			if(health < 0){
 				Application.LoadLevel(1);
 			}
 		}
+	}
+	void Start(){
+		maxHealth = health;
 	}
 	public override void DealDamage(Vector3 damage) {
 		currentHealth -= damage.magnitude;
