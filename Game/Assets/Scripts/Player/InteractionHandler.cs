79e4e1292bb897a34dd1f184ea0be119ddc24b80
M124 <m.lewandowski124@gmail.com>
2014-05-06 12:07:26 +0200

    avoid or destroy blockades

--- Game/Assets/Scripts/Player/InteractionHandler.cs
+++ Game/Assets/Scripts/Player/InteractionHandler.cs
@@ -1,12 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 public class InteractionHandler : MonoBehaviour {
     public float touchRange = 0.7f;
     public float touchRadius = 0.2f;
     private string message;
     Vector2 screenPosition;
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
     void Update() {
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
         RaycastHit hitInfo;
         message = null;
         if (Physics.SphereCast(transform.position, touchRadius, transform.forward, out hitInfo, touchRange)) {
@@ -14,7 +39,8 @@ public class InteractionHandler : MonoBehaviour {
             if (interactiveObject != null) {
                 message = interactiveObject.message;
                 screenPosition = Camera.main.WorldToScreenPoint(interactiveObject.transform.position);
-                if (Input.GetKeyDown(KeyCode.F)) {
+                if (Input.GetKeyDown(KeyCode.F) || state.Buttons.X == ButtonState.Pressed)
+                {
                     interactiveObject.Action();
                 }
             }
