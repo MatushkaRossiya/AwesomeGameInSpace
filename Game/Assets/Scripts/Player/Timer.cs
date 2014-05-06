e869d832d2f1eb7b939cedf7f3bef444f3c53bfe
jakubdziworski <jakub.dziworski@gmail.com>
2014-05-06 10:13:21 +0200

    HUD

---
+++ Game/Assets/Scripts/Player/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float beginTime; //time in seconds
+	float time;
+	// Use this for initialization
+	void Start () {
+		time = beginTime;
+	}
+	public float getTime(){
+		return time;
+	}
+	public override string ToString(){
+		int minuty = Mathf.FloorToInt(time / 60.0f);
+		int sekundy = (int)((time/60.0f - minuty)*60);
+		if(sekundy < 10) return minuty.ToString() + ":0" + sekundy.ToString();
+		return minuty.ToString() + ":" + sekundy.ToString();
+	}
+	public void setTime(float inpTime){
+		time = inpTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(time > Time.deltaTime)
+		time -= Time.deltaTime;
+	}
+}
