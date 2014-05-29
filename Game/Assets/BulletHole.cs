2def12f1f7b5f5975e60a41f9ea7f630353389eb
romanchom <romanchom@gmail.com>
2014-05-29 23:32:41 +0200

    - Awesome glowing bulletholes added

---
+++ Game/Assets/BulletHole.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletHole : MonoBehaviour {
+	public float brightness;
+	Material mat;
+	int ID;
+	void Start () {
+		mat = renderer.material;
+		ID = Shader.PropertyToID("_EmissionLM");
+	}
+	
+	void Update () {
+		brightness -= Time.deltaTime * 0.5f;
+		if (brightness <= 0) Destroy(this);
+		mat.SetFloat(ID, brightness);
+	}
+}
