bb50c3a215f34420173382a2f792114c4433b126
romanchom <romanchom@gmail.com>
2014-06-04 19:05:28 +0200

    - Aliens bleed

---
+++ Game/Assets/Scripts/Enemy/AlienFleshMaterial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienFleshMaterial : MaterialProperties {
+	public GameObject bloodParticlesPrefab;
+	
+	private static ParticleSystem bloodParticles;
+	
+	
+	public override void Hit(RaycastHit hit, Vector3 force) {
+		try {
+			Emit(hit, force);
+		}
+		catch {
+			bloodParticles = ((GameObject)Instantiate(bloodParticlesPrefab)).GetComponent<ParticleSystem>();
+			Emit(hit, force);
+		}
+	}
+
+	void Emit(RaycastHit hit, Vector3 force) {
+		bloodParticles.transform.position = hit.point;
+		bloodParticles.transform.forward = -force.normalized;
+		bloodParticles.Emit(20);
+	}
+}
