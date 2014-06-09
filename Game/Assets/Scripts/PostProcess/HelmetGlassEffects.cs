e808bccbd8f338b2f602772b714f19fa9b97eaec
romanchom <romanchom@gmail.com>
2014-06-09 16:15:13 +0200

    - Finally got my steamy steam shader working.

--- Game/Assets/Scripts/PostProcess/HelmetGlassEffects.cs
+++ Game/Assets/Scripts/PostProcess/HelmetGlassEffects.cs
@@ -1,19 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class HelmetGlassEffects : MonoBehaviour {
-	public Material postProcessMaterial;
 	public Camera helmetEffectCamera;
+	public Shader refractionShader;
+	public Shader blurShader;
+	public Texture steamTexture;
+	public Texture noise;
+	[Range(0, 1)]
+	public float steaminess;
+	[Range(0, 3)]
+	public int downSample;
+	[Range(1, 5)]
+	public int blurIteratons;
+	[Range(0.001f, 0.1f)]
+	public float blurSize;
+	[Range(0.0001f, 0.005f)]
+	public float refractionSampleSize;
+	[Range(-0.001f, 0.001f)]
+	public float refractionMagnitude;
+	
+	private Material _refractionMat;
+	private Material refractionMat{
+		get{
+			if(_refractionMat == null){
+				_refractionMat = new Material(refractionShader);
+				refractionMat.SetTexture("_Glass", helmetEffectCamera.targetTexture);
+				refractionMat.SetTexture("_Steam", steamTexture);
+			}
+			return _refractionMat;
+		}
+	}
 
-	void Start() {
-		RenderTexture tex = new RenderTexture(Screen.width, Screen.height, 24);
-		helmetEffectCamera.targetTexture = tex;
-		postProcessMaterial.SetTexture("_Glass", tex); 
+	private Material _blurMat;
+	private Material blurMat{
+		get{
+			if (_blurMat == null) {
+				_blurMat = new Material(blurShader);
+				_blurMat.SetTexture("_Steam", steamTexture);
+			}
+			return _blurMat;
+		}
+	}
+
+	void Init() {
+		if (helmetEffectCamera.targetTexture == null) {
+			helmetEffectCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		}
+	}
+
+	void Release() {
+		if (refractionMat != null) {
+			DestroyImmediate(_refractionMat);
+		}
+		if (blurMat != null) {
+			DestroyImmediate(_blurMat);
+		}
+		if (helmetEffectCamera != null && helmetEffectCamera.targetTexture != null) {
+			DestroyImmediate(helmetEffectCamera.targetTexture);
+			helmetEffectCamera.targetTexture = null;
+		}
 	}
 	
 	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		Init();
+		int actualWidth = Screen.width >> downSample;
+		int actualHeight = Screen.height >> downSample;
 
-		//mat is the material containing your shader
-		Graphics.Blit(source, destination, postProcessMaterial);
+
+		float size = blurSize;
+		RenderTexture blurred1 = RenderTexture.GetTemporary(actualWidth, actualHeight, 0, source.format);
+		RenderTexture blurred2 = RenderTexture.GetTemporary(actualWidth, actualHeight, 0, source.format);
+
+		float aspect = (float) Screen.height / (float) Screen.width;
+		blurMat.SetFloat("_Size", size);
+		blurMat.SetFloat("_Steaminess", steaminess);
+		Graphics.Blit(source, blurred1, blurMat, 0);
+		Graphics.Blit(blurred1, blurred2, blurMat, 1);
+		for (int i = 1; i < blurIteratons; ++i) {
+			size *= Mathf.Pow(2, -1.0f / (blurIteratons - 1));
+			blurMat.SetFloat("_Size", size);
+			Graphics.Blit(blurred1, blurred2, blurMat, 0);
+			blurMat.SetFloat("_Size", size / aspect);
+			Graphics.Blit(blurred2, blurred1, blurMat, 1);
+		}
+		refractionMat.SetTexture("_Blurred", blurred1);
+
+		refractionMat.SetFloat("_Delta", refractionSampleSize);
+		refractionMat.SetFloat("_Magnitude", refractionMagnitude);
+		refractionMat.SetFloat("_Steaminess", steaminess);
+
+		RenderTexture.ReleaseTemporary(blurred2);
+		Graphics.Blit(source, destination, refractionMat);
+		RenderTexture.ReleaseTemporary(blurred1);
+	}
+
+	void OnDisable() {
+		Release();
 	}
 }
---
+++ Game/Assets/Shaders/SimpleGaussianBlur.shader
@@ -0,0 +1,79 @@
+﻿Shader "Hidden/SimpleGaussian" {
+	Properties {
+		_MainTex ("Screen", 2D) = "white" {}
+		_Size  ("Blur Size", float) = 0.001
+		_Steam ("Steam", 2D) = "white" {}
+		_Steaminess ("Steaminess", range(0, 1)) = 0
+	}
+
+
+	CGINCLUDE
+	#include "UnityCG.cginc" 
+	//we include "UnityCG.cginc" to use the appdata_img struct
+    
+	struct v2f {
+		float4 pos : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+	
+	sampler2D _MainTex;
+	float _Size;
+	sampler2D _Steam;
+	float _Steaminess;
+   
+	//Our Vertex Shader 
+	v2f vert (appdata_img v){
+		v2f o;
+		o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		o.uv = v.texcoord.xy;
+		return o; 
+	}
+	ENDCG
+
+
+	SubShader {
+		ZTest Always Cull Off ZWrite Off Fog { Mode Off } //Rendering settings
+		
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+	
+			float4 frag (v2f i) : COLOR{
+				float4 sum = float4(0, 0, 0, 0);
+
+				float size = _Size * saturate((_Steaminess - tex2D(_Steam, i.uv).r ) * 5);
+				sum += tex2D(_MainTex, i.uv + float2(size * -2, 0)) * 0.06136;
+				sum += tex2D(_MainTex, i.uv + float2(size * -1, 0)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv) * 0.38774;
+				sum += tex2D(_MainTex, i.uv + float2(size * 1, 0)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv + float2(size * 2, 0)) * 0.06136;
+
+				return sum;
+			}
+			ENDCG
+		}
+
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			    
+			float4 frag (v2f i) : COLOR{
+				float4 sum = float4(0, 0, 0, 0);
+				
+				float size = _Size * saturate((_Steaminess - tex2D(_Steam, i.uv).r) * 5);
+
+				sum += tex2D(_MainTex, i.uv + float2(0, size * -2)) * 0.06136;
+				sum += tex2D(_MainTex, i.uv + float2(0, size * -1)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv) * 0.38774;
+				sum += tex2D(_MainTex, i.uv + float2(0, size * 1)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv + float2(0, size * 2)) * 0.06136;
+
+				return sum;
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
