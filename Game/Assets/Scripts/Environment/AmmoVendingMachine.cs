0a4b2d5e0c97ddb2ea7687db537165d806e79fcc
MatushkaRossiya <MatushkaRossiya@email.com>
2014-06-11 23:22:55 +0200

    Added grenade vending machine.

--- Game/Assets/Scripts/Environment/AmmoVendingMachine.cs
+++ Game/Assets/Scripts/Environment/AmmoVendingMachine.cs
@@ -1,37 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AmmoVendingMachine : Interactive {
-	
-    public int ammoPrice;
+public class AmmoVendingMachine : Interactive
+{
+	public int ammoPrice;
 	public int ammoAmount;
-	public override string message {
-		get {
+
+	public override string message
+	{
+		get
+		{
 			string ret;
 			CanBuy(out ret);
 			return ret;
 		}
 	}
 
-	public override void MomentaryAction() {
+	public override void MomentaryAction()
+	{
 		string str;
-		if (CanBuy(out str)) {
+		if (CanBuy(out str))
+		{
 			LaserRifle.instance.ammo += ammoAmount;
 			PlayerStats.instance.syf -= ammoPrice;
 		}
 	}
 
-	private bool CanBuy(out string msg) {
-        if (LaserRifle.instance.ammo >= LaserRifle.instance.maxAmmo) {
+	private bool CanBuy(out string msg)
+	{
+		if (LaserRifle.instance.ammo >= LaserRifle.instance.maxAmmo)
+		{
 			msg = "Ammo full";
 			return false;
 		}
-		else if (PlayerStats.instance.syf < ammoPrice) {
-			msg = "Insufficient syf";
+		else if (PlayerStats.instance.syf < ammoPrice)
+		{
+			msg = "Insufficient Syf";
 			return false;
 		}
-		else {
-			msg = string.Format("Buy {0} ammo for {1} syf", ammoAmount, ammoPrice);
+		else
+		{
+			msg = string.Format("Buy {0} ammo for {1} Syf", ammoAmount, ammoPrice);
 			return true;
 		}
 	}
---
+++ Game/Assets/Scripts/Environment/GrenadeVendingMachine.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeVendingMachine : Interactive
+{
+	public int grenadePrice;
+	public int grenadeAmount;
+
+	public override string message
+	{
+		get
+		{
+			string ret;
+			CanBuy(out ret);
+			return ret;
+		}
+	}
+	
+	public override void MomentaryAction()
+	{
+		string str;
+		if (CanBuy(out str))
+		{
+			LaserRifle.instance.grenades += grenadeAmount;
+			PlayerStats.instance.syf -= grenadePrice;
+		}
+	}
+	
+	private bool CanBuy(out string msg)
+	{
+		if (!LaserRifle.instance.hasGrenadeLaucherUpgrade)
+		{
+			msg = "You don't have grenade launcher";
+			return false;
+		}
+		if (LaserRifle.instance.grenades >= LaserRifle.instance.maxGrenades)
+		{
+			msg = "Grenades full";
+			return false;
+		}
+		else if (PlayerStats.instance.syf < grenadePrice)
+		{
+			msg = "Insufficient Syf";
+			return false;
+		}
+		else
+		{
+			msg = string.Format("Buy {0} grenade for {1} Syf", grenadeAmount, grenadePrice);
+			return true;
+		}
+	}
+}
