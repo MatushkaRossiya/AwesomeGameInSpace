bb02a7e95aeaf128c33c79a2c6bcb70bcd50f26d
MatushkaRossiya <MatushkaRossiya@email.com>
2014-06-12 23:20:07 +0200

    Location 1 - shadows and new alarm lights.

---
+++ Game/Assets/Scripts/Environment/AlarmLight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour
+{
+	
+	void Start()
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		transform.Rotate(0.0f, 45.0f * Time.fixedDeltaTime, 0.0f);
+	}
+}
