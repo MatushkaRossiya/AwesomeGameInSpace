27fcd1ee3c74c80e53ae270fbb5a95eed697b35a
romanchom <romanchom@gmail.com>
2014-06-20 19:45:09 +0200

    - Location 5 usability works WIP

--- Game/Assets/Scripts/Environment/MaterialHolder.cs
+++ Game/Assets/Scripts/Environment/MaterialHolder.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Collider))]
 public class MaterialHolder : MonoBehaviour
 {
     public MaterialProperties materialProperties;
