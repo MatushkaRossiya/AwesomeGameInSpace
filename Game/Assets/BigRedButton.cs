a7a96e893c670c6fd27da27e9efd3b138f900764
romanchom <romanchom@gmail.com>
2014-06-24 18:14:34 +0200

    - BRB

---
+++ Game/Assets/BigRedButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigRedButton : Interactive {
+	public AudioClip voice1;
+	public AudioClip voice2;
+	public Light escapePodLight;
+
+	public AudioSource voiceSource;
+	public AudioSource sirenSource;
+	public AudioSource explosionSource;
+
+	public ParticleSystem giantExplosionParticles;
+
+
+	public override string message {
+		get { return "PUSH ME!!!!"; }
+	}
+
+	public override void MomentaryAction() {
+		StartCoroutine(selfDestruct());
+	}
+
+	IEnumerator selfDestruct() {
+		yield return new WaitForSeconds(1.0f);
+		voiceSource.clip = voice1;
+		voiceSource.Play();
+		escapePodLight.color = Color.red;
+		yield return new WaitForSeconds(voice1.length);
+		sirenSource.Play();
+		yield return new WaitForSeconds(3.0f);
+		voiceSource.clip = voice2;
+		voiceSource.Play();
+		yield return new WaitForSeconds(voice2.length);
+		explosionSource.Play();
+		giantExplosionParticles.enableEmission = true;
+		// TODO delete saved games
+	}
+}
