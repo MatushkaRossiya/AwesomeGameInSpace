3f186d4311651a79e727371dfaf29cafb85161b3
MatushkaRossiya <MatushkaRossiya@email.com>
2014-06-25 01:40:33 +0200

    Scratches during day.

---
+++ Game/Assets/Scripts/Environment/Scratch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scratch : MonoBehaviour
+{
+	public AudioClip[] scratches;
+
+	private float scratchLenght;
+	private float pauseLenght;
+	private float time = 0.0f;
+	private bool playing = false;
+	private int scratchIndex;
+	private int scratchesLenght;
+
+	void Start()
+	{
+		scratchesLenght = scratches.Length;
+	}
+
+	void FixedUpdate()
+	{
+		if (!playing)
+		{
+			time = 0.0f;
+			playing = true;
+			scratchIndex = Random.Range(0, scratchesLenght);
+			audio.clip = scratches[scratchIndex];
+			scratchLenght = scratches[scratchIndex].length;
+			float pauseModifier = 1.0f - audio.volume;
+			pauseLenght = Random.Range(2.0f * pauseModifier + 0.1f, 5.0f * pauseModifier + 0.1f);
+			audio.Play();
+		}
+		else
+		{
+			time += Time.fixedDeltaTime;
+			if (time > scratchLenght + pauseLenght)
+			{
+				playing = false;
+			}
+			if (time > scratchLenght)
+			{
+				audio.Stop();
+			}
+		}
+	}
+}
---
+++ Game/Assets/Scripts/Environment/ScratchesManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScratchesManager : MonoSingleton<ScratchesManager>
+{
+	public float dayPhase
+	{
+		set
+		{
+			_dayPhase = value;
+			for (int i = 0; i < scratches.Length; ++i)
+			{
+				scratches [i].volume = (_dayPhase / GameMaster.instance.dayLenght) + 0.1f;
+			}
+		}
+	}
+	private float _dayPhase;
+
+	private AudioSource[] scratches;
+	
+	void Start()
+	{
+	
+	}
+
+	public void TurnScratches(bool on)
+	{
+		for (int i = 0; i < scratches.Length; ++i)
+		{
+			scratches [i].enabled = on;
+		}
+	}
+
+	public void FindScratches()
+	{
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("Scratch");
+		if (temp != null)
+		{
+			scratches = new AudioSource[temp.Length];
+			for (int i = 0; i < temp.Length; ++i)
+			{
+				AudioSource scratch = temp [i].GetComponent<AudioSource>();
+				if (scratch != null)
+				{
+					scratches [i] = scratch;
+				}
+			}
+		}
+	}
+
+	public override void Init()
+	{
+		FindScratches();
+		dayPhase = 0.0f;
+	}
+}
