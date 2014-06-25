1f5968b9d0624cda4b25741eeab218827e96eeb8
Adam Wiśniewski <adam.wisniewski93@gmail.com>
2014-06-25 12:56:17 +0200

    koniec

---
+++ Game/Assets/Scripts/Environment/CowSpin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowSpin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler (new Vector3 (0.0f, this.transform.rotation.y + 0.01f, 0.0f));
+	}
+}
